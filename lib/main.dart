41eb0c8a85c66c112d8877d7dc92a6ff6d2cb580
seoziyeong <bestzenio@gmail.com>
2023-09-30 20:01:39 +0900

    section: webview (ver 4.x.x)

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,16 @@
+import 'package:codefactory_flutter/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  // flutter 프레임워크가 앱을 실행할 준비가 될 때까지 기다림
+  // main()안에 runApp밖에 없으면 자동으로 실행되는데,
+  // WebView를 쓰기 위해서 필요한 설정
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(
     const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        body: Center(
-          child: Text(
-            "Hello World",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 24,
-            ),
-          ),
-        ),
-      ),
+      home: HomeScreen(),
     ),
   );
 }
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+// home url 설정
+// loadRequest에 String이 아닌 Uri 타입이 들어가야 하므로, Uri 파싱
+final homeUrl = Uri.parse("https://velog.io/@berrry");
+
+class _HomeScreenState extends State<HomeScreen> {
+  // WebViewController 설정
+  final WebViewController _webViewController = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted) // js 허용
+    ..loadRequest(homeUrl); // 초기화면 설정
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: const Text("🍓"),
+        centerTitle: true,
+        backgroundColor: Colors.grey,
+        actions: [
+          IconButton(
+            onPressed: () {
+              _webViewController.loadRequest(homeUrl); // 홈(초기화면)으로 이동
+            },
+            icon: const Icon(Icons.home),
+          )
+        ],
+        elevation: 0,
+      ),
+      body: WebViewWidget(
+        controller: _webViewController, // WebViewWidget에 컨트롤러만 달아주면 됨
+      ),
+    );
+  }
+}
