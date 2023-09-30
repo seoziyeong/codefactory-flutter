81f304d7ce60bc04c970fe19ca918372394a64b7
seoziyeong <bestzenio@gmail.com>
2023-09-30 18:31:40 +0900

    chore: debugShowCheckedModeBanner false

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.black,
         body: Center(
